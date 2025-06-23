08be363322daf2d36226cd6e6b1cbc609b323454
Giovane <giovxnedamazio@gmail.com>
2025-06-23 09:58:44 -0300

    full outer join, right join e left join conceito

--- TestesSQL/sql/FULLOUTERJOIN.sql
+++ TestesSQL/sql/FULLOUTERJOIN.sql
@@ -9,4 +9,29 @@ simplesmente irá preencher esses valores com "null".
 SELECT FROM * TabelaA
 FULL OUTER JOIN TabelaB
 ON TabelaA.nome = TabelaB.Nome
--- Isso vai retornar os nomes nas tabelas
+/*
+Esse comando vai retornar tudo que existe na TabelaA,
+na TabelB e também tudo que existe em comum entre elas
+*/
+
+-- LEFT OUTER JOIN
+/*
+Left Outer Join retorna um conjunto de todos os registros
+da TabelaA e os registros correspondentes (quando disponíveis)
+na TabelaB. Se não houver registros correspondentes ele
+vai preencher com "null".
+Ou seja, ele é um Full Outer Join, porém caso não tenha
+dados na TabelaA ele não vai trazer.
+*/
+
+-- Exemplo SQL:
+SELECT * FROM TabelaA
+LEFT JOIN TabelaB
+ON TabelaA.Nome = TabelaB.Nome;
+
+/*
+Existe também o RIGHT OUTER JOIN, que basicamente apenas
+altera o lado, ou seja, caso pegue o exemplo citado acima
+ele usaria como base a TabelaB, o conceito não muda,
+apenas troca o lado.
+*/
