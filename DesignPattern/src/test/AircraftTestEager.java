55bddb1eef090d692acf7daf7d9087ea104bbf58
Giovane <giovxnedamazio@gmail.com>
2025-07-02 08:23:41 -0300

    design pattern singletonEager and singletonLazy(WRONG)

---
+++ DesignPattern/src/test/AircraftTestEager.java
@@ -0,0 +1,15 @@
+package test;
+
+import Singleton.Aircraft;
+
+public class AircraftTest {
+    public static void main(String[] args) {
+        bookSeat("1A");
+        bookSeat("1A");
+    }
+
+    private static void bookSeat(String seat) {
+        Aircraft aircraft = new Aircraft("787-900");
+        System.out.println(aircraft.bookSeat(seat));
+    }
+}
