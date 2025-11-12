8e18721c2e762167394ab058c3b40caf359ec553
Giovane Damazio <giovxnedamazio@gmail.com>
2025-11-11 22:52:54 -0300

    test

--- productapi/src/main/java/com/product/productapi/ProductApiApplication.java
+++ productapi/src/main/java/com/product/productapi/ProductApiApplication.java
@@ -2,9 +2,17 @@ package com.product.productapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProductApiApplication {
+
+	@GetMapping("/test") // url
+	public String Test(){
+		return "Teste1";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApiApplication.class, args);
