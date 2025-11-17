6951134d8481e8ecc8e4a2bdbf5c0647fbc99e31
Giovane Damazio <giovxnedamazio@gmail.com>
2025-11-17 18:58:00 -0300

    initial commit

---
+++ springarchitecture/src/test/java/io/github/giodmz/springarchitecture/SpringarchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.giodmz.springarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringarchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
