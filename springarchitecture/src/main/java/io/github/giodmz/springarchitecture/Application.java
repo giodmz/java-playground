6c8ff21fb51028bda97cb2373899163b2146a942
Giovane Damazio <giovxnedamazio@gmail.com>
2025-11-17 19:20:47 -0300

    app.run tests

---
+++ springarchitecture/src/main/java/io/github/giodmz/springarchitecture/Application.java
@@ -0,0 +1,32 @@
+package io.github.giodmz.springarchitecture;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		// SpringApplication.run(Application.class, args);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+		builder.bannerMode(Banner.Mode.OFF); // remove o banner do spring
+		builder.profiles("1", "2");
+		builder.run(args);
+
+
+		// app context
+		ConfigurableApplicationContext appContext = builder.context();
+		// var productRepository = appContext.getBean("productRepository");
+		builder.properties("spring.datasource.url=jdbc://");
+
+		ConfigurableEnvironment environment = appContext.getEnvironment(); // can read any properties
+		String appName = environment.getProperty("spring.application.name");
+		System.out.println("App name: " + appName);
+	}
+
+}
