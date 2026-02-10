06b37c328b90b8cf0fd2e995fe46685ed36e8fe2
Giovane Damazio <giovxnedamazio@gmail.com>
2026-02-10 12:58:38 -0300

    bean gerenciado

---
+++ springarchitecture/src/main/java/io/github/giodmz/springarchitecture/BeanGerenciado.java
@@ -0,0 +1,28 @@
+package io.github.giodmz.springarchitecture;
+
+import io.github.giodmz.springarchitecture.todos.TodoEntity;
+import io.github.giodmz.springarchitecture.todos.TodoValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanGerenciado {
+
+    @Autowired
+    private TodoValidator validator;
+
+    public BeanGerenciado(TodoValidator validator) {
+        this.validator = validator;
+    }
+
+    public void utilizar() {
+        var todo = new TodoEntity();
+        validator.validar(todo);
+    }
+
+    @Autowired
+    public void setValidator(TodoValidator validator){
+        this.validator = validator;
+    }
+
+}
