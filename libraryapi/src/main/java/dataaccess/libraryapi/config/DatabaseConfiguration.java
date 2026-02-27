eb14832c207512692c373cfb74509a5acf81f05d
Giovane Damazio <giovxnedamazio@gmail.com>
2026-02-27 13:59:44 -0300

    db conn

---
+++ libraryapi/src/main/java/dataaccess/libraryapi/config/DatabaseConfiguration.java
@@ -0,0 +1,50 @@
+package dataaccess.libraryapi.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    }
+
+    // ja vem com spring data jpa
+    @Bean
+    public DataSource hikariDataSource(){
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10); // vai liberar no máximo 10 conexões nessa pool
+        config.setMinimumIdle(1); // tamanho mínimo da pool
+        config.setPoolName("library-db-pool"); // nome da pool
+        config.setMaxLifetime(600000); // tempo máximo de uma conexão (em ms)
+
+        return new HikariDataSource(config);
+    }
+}
