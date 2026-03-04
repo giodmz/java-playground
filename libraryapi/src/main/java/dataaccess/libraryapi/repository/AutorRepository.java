673e049aecf40593c27365d3de2ccce1b143b42e
Giovane Damazio <giovxnedamazio@gmail.com>
2026-03-04 17:25:43 -0300

    repository livro e autor

---
+++ libraryapi/src/main/java/dataaccess/libraryapi/repository/AutorRepository.java
@@ -0,0 +1,10 @@
+package dataaccess.libraryapi.repository;
+
+import dataaccess.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+
+}
---
+++ libraryapi/src/main/java/dataaccess/libraryapi/repository/LivroRepository.java
@@ -0,0 +1,10 @@
+package dataaccess.libraryapi.repository;
+
+import dataaccess.libraryapi.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LivroRepository extends JpaRepository<Livro, UUID> {
+    
+}
