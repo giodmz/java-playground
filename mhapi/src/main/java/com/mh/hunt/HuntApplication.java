3560e2b643c7b7ded7d8473f10cd9539243f7060
Giovane Damazio <giovxnedamazio@gmail.com>
2026-03-07 12:52:31 -0300

    initial commit

---
+++ mhapi/src/main/java/com/mh/hunt/HuntApplication.java
@@ -0,0 +1,13 @@
+package com.mh.hunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuntApplication.class, args);
+	}
+
+}
---
+++ mhapi/src/test/java/com/mh/hunt/HuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh.hunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
